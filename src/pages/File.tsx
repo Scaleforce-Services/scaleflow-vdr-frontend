bfd3f933ca7338f6cde4b328e0a68649145677e5
Indroneel Ray <indroneel7772001@gmail.com>
2025-05-15 15:10:15 +0530

    ✨ Update the file download page such that:    - It first extracts the file path from the url to check whether the file exists or not    - If it doesn't is hows a 404 icon in red    - If it does find it, it shows a file icon with    the file name below it    - A download file button appears    - Clicking on it will show a progress bar    that updates as the file download progresses    - Once it is complete, the file downloads to    your local system

--- src/pages/File.tsx
+++ src/pages/File.tsx
@@ -1,34 +1,78 @@
 import React from 'react';
-import { getUrl } from 'aws-amplify/storage';
+import { downloadData, getProperties } from 'aws-amplify/storage';
 import { useNavigate, useParams } from 'react-router';
-import { BUCKET_NAME } from '../components/CustomStorageBrowser';
-import { Button, Flex, Heading, View, Text } from '@aws-amplify/ui-react';
+import { Button, Flex, Heading, View, Text, Loader } from '@aws-amplify/ui-react';
 import { FcDocument } from 'react-icons/fc';
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
+import { TbError404 } from 'react-icons/tb';
 
 export default function File() {
   const { fileName } = useParams();
-  const [fileLink, setFileLink] = React.useState('');
   const navigate = useNavigate();
+  const [isError, setIsError] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(true);
+  const [isDownloading, setDownloading] = React.useState(false);
+  const downloadProgressRef = React.useRef<SVGSVGElement>(null);
 
-  const getFileLink = (fileName: string) => {
-    getUrl({
-      path: fileName,
-      options: {
-        bucket: BUCKET_NAME,
-      },
-    })
-      .then((result) => {
-        setFileLink(result.url.toString());
-      })
-      .catch((error) => {
-        console.error('Error getting file link:', error);
+  const downloadFile = async (fileName: string | undefined) => {
+    if (!fileName) return;
+    setDownloading(true);
+    const progressBarRef = downloadProgressRef.current?.querySelector('svg g line:nth-child(2)');
+    console.log(progressBarRef)
+    progressBarRef?.setAttribute('style', 'stroke: var(--amplify-colors-green-60)');
+    try {
+      // Download a file from S3 bucket
+      const data = await downloadData({
+        path: fileName,
+        options: {
+          onProgress: (progress) => {
+            if (!progress.totalBytes) return;
+            const completionRate = Math.round(
+              (progress.transferredBytes / progress.totalBytes) * 100
+            );
+            progressBarRef?.setAttribute('x2', `${completionRate}%`);
+          },
+        },
+      }).result;
+
+      // Get blob data from the response
+      const blob = await data.body.blob();
+
+      //Create an object url
+      const fileUrl = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = fileUrl;
+      link.download = fileName.split('/').pop() || 'download';
+      document.body.appendChild(link);
+
+      // Trigger the download
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.debug('Error downloading file:', error);
+    } finally {
+      setDownloading(false);
+    }
+  };
+
+  const checkIfFileExists = async (fileName: string) => {
+    setLoading(true);
+    try {
+      await getProperties({
+        path: fileName,
+        options: {},
       });
+    } catch (error) {
+      console.debug(error);
+      setIsError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   React.useEffect(() => {
     if (fileName) {
-      getFileLink(fileName);
+      checkIfFileExists(fileName);
     }
   }, [fileName]);
 
@@ -41,14 +85,50 @@ export default function File() {
         <Text>Back to files</Text>
       </Button>
       <Flex direction="column" alignItems="center" justifyContent="center" height="100%">
-        <FcDocument size={100} />
-        <Heading level={5}>{trimmedFileName}</Heading>
-        {fileLink && (
-          <a href={fileLink} target="_blank" rel="noopener noreferrer">
-            <h3>Click here to download the file</h3>
-          </a>
+        {isLoading ? (
+          <>
+            <Loader size="large" />
+            <Text>Loading...</Text>
+          </>
+        ) : isError ? (
+          <>
+            <Flex
+              border="4px solid red"
+              borderRadius="100%"
+              padding="0.5rem"
+              width="9rem"
+              height="9rem"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <TbError404 size={100} color="red" />
+            </Flex>
+            <Heading level={6} color="red.50">
+              File not found
+            </Heading>
+            <Heading level={5}>{trimmedFileName}</Heading>
+          </>
+        ) : (
+          <>
+            <FcDocument size={100} />
+            <Heading level={5}>{trimmedFileName}</Heading>
+
+            <Button variation="link" onClick={() => downloadFile(fileName)}>
+              Click here to download the file
+            </Button>
+
+              <Loader
+                size="large"
+                variation="linear"
+                isDeterminate={true}
+                percentage={0}
+                ref={downloadProgressRef}
+                isPercentageTextHidden={true}
+                width="200px"
+                opacity={isDownloading ? 1 : 0}
+              />
+          </>
         )}
-        {!fileLink && <h3>Loading...</h3>}
       </Flex>
     </View>
   );
